656416c00d3a83fd3a84fd05fdb393c2f7219279
itaydafna <itaydafna@gmail.com>
2016-07-23 12:09:39 +0300

    game and flaging working

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Cell() {
     this.cellNum = 0;
     this.neighborMinesCounter = 0;
     this.mine = (function () {
-        if (Math.round(0.75*Math.random())) {
+        if (Math.round(0.6*Math.random())) {
             this.el.className = "mine";
             return true
         } else {
@@ -16,16 +16,17 @@ function Cell() {
     this.Lid = function(){
         this.el = document.createElement("div");
         this.el.className = "lid";
+        this.el.dataset.flagged = "no";
         //event which indicates lid has been take off cell
-        this.lidOff = new Event("lidOff");
+        // this.lidOff = new Event("lidOff");
     };
 
     //method which removes the lid from cell and dispatches "lidOff" event
-    this.Lid.prototype.removeLid = function () {
-        this.el.parentNode.dispatchEvent(this.lidOff);
-        this.el.parentNode.removeChild(this.el);
-
-    };
+    // this.Lid.prototype.removeLid = function () {
+    //     this.el.parentNode.dispatchEvent(this.lidOff);
+    //     this.el.parentNode.removeChild(this.el);
+    //
+    // };
 
 }
 
@@ -60,7 +61,8 @@ GameBoard.prototype.createGameBoard = function (gridSize) {
         for (var j = 0; j < gridSize; j++) {
             var cell = new Cell();
             var lid = new cell.Lid();
-            lid.el.addEventListener("click",cell.Lid.prototype.removeLid.bind(lid));
+            lid.el.addEventListener("click",removeLid);
+            lid.el.addEventListener("contextmenu",flagToggle);
             cell.el.dataset.cell = j;
             cell.el.addEventListener("lidOff",afterLidOff.bind(cell));
             cell.el.appendChild(lid.el);
@@ -185,6 +187,20 @@ function numberEmptyCells(table){
 
 
 
+
+//event which indicates lid has been take off cell
+var lidOff = new Event("lidOff");
+
+//function which removes lid from cell and dispatches "lidOff" event
+function removeLid(event){
+    event.target.parentNode.dispatchEvent(lidOff);
+    if (event.target.parentNode !== null) {
+        event.target.parentNode.removeChild(event.target);
+    }
+};
+
+
+
 //this function detrmines what happens after a lid-off event
 // it takes the td element of the cell which dispatched the event.
 
@@ -193,13 +209,154 @@ function afterLidOff(event){
         alert ("game over!");
     } else if
     (event.target.dataset.numInCell === "0"){
-     alert("need to think how to write this..")
+        lidOffNeigborCells(event.target);
+
     }
 
 }
 
+//this function takes lid off (runs lidOff on it) from all neighbors of "0" cells
+
+function lidOffNeigborCells(td) {
+    var rowNumber = Number(td.parentNode.dataset.row),
+    cellNumber = Number(td.dataset.cell);
+
+//top-left neighbor
+    var topLeft = document.body.querySelector("tr[data-row ='" + [rowNumber - 1] + "'] td[data-cell='" + [cellNumber - 1] + "']");
+    if (!topLeft) {
+
+    } else {
+        var childLid = topLeft.querySelector(".lid");
+        if (childLid!==null) {
+            topLeft.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            topLeft.dispatchEvent(lidOff);
+        }
+
+    }
+
+//top neighbor
+    var top = document.body.querySelector("tr[data-row ='" + [rowNumber - 1] + "'] td[data-cell='" + cellNumber + "']");
+    if (!top) {
+
+    } else {
+        var childLid = top.querySelector(".lid");
+        if (childLid!==null) {
+            top.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            top.dispatchEvent(lidOff);
+        }
+    }
 
 
+
+// top-right neighbor
+    var topRight = document.body.querySelector("tr[data-row ='" + [rowNumber - 1] + "'] td[data-cell='" + [cellNumber + 1] + "']");
+    if (!topRight) {
+
+    } else{
+        var childLid = topRight.querySelector(".lid");
+        if (childLid!==null) {
+            topRight.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            topRight.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+// left neighbor
+    var left = document.body.querySelector("tr[data-row ='" + rowNumber + "'] td[data-cell='" + [cellNumber - 1] + "']");
+    if (!left) {
+
+    } else{
+        var childLid = left.querySelector(".lid");
+        if (childLid!==null) {
+            left.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            left.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+// right neighbor
+    var right = document.body.querySelector("tr[data-row ='" + rowNumber + "'] td[data-cell='" + [cellNumber + 1] + "']");
+    if (!right) {
+
+    } else{
+        var childLid = right.querySelector(".lid");
+        if (childLid!==null) {
+            right.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            right.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+// bottom-left neighbor
+    var bottomLeft = document.body.querySelector("tr[data-row ='" + [rowNumber + 1] + "'] td[data-cell='" + [cellNumber - 1] + "']");
+    if (!bottomLeft) {
+
+    } else{
+        var childLid = bottomLeft.querySelector(".lid");
+        if (childLid!==null) {
+            bottomLeft.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            bottomLeft.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+// bottom neighbor
+    var bottom = document.body.querySelector("tr[data-row ='" + [rowNumber + 1] + "'] td[data-cell='" + cellNumber + "']");
+    if (!bottom) {
+
+    } else{
+        var childLid = bottom.querySelector(".lid");
+        if (childLid!==null) {
+            bottom.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            bottom.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+//bottom-right neighbor
+    var bottomRight = document.body.querySelector("tr[data-row ='" + [rowNumber + 1] + "'] td[data-cell='" + [cellNumber + 1] + "']");
+    if (!bottomRight) {
+
+    } else {
+        var childLid = bottomRight.querySelector(".lid");
+        if (childLid!==null) {
+            bottomRight.removeChild(childLid);
+            var lidOff = new Event("lidOff");
+            bottomRight.dispatchEvent(lidOff);
+        }
+    }
+
+
+
+}
+
+
+function flagToggle(event){
+    event.preventDefault();
+    if (event.target.dataset.flagged === "no"){
+        event.target.dataset.flagged = "yes";
+        var flag = document.createElement("div");
+        flag.className = "flag";
+        event.target.appendChild(flag);
+    } else if
+    (event.target.className === "flag"){
+        event.target.parentNode.dataset.flagged = "no";
+        event.target.parentNode.removeChild(event.target);
+    }
+
+}
 
 
 //this gets everything started
